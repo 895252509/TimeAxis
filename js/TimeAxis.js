127ebe720ebe00b3001ee9e15de8fb856cb8ed36
zhou xiang <zhouxiang@trans-cosmos.com.cn>
2017-08-03 15:19:18 +0800

    slider move

--- js/TimeAxis.js
+++ js/TimeAxis.js
@@ -1,3 +1,23 @@
+/**
+ * 扩展SVG Dom方法
+ * 
+ */
+SVGPolygonElement.z_baseX = 0;
+SVGPolygonElement.z_baseY = 0;
+
+SVGPolygonElement.prototype.z_moveTo = function (_x,_y){
+
+
+}
+
+/**
+ * 定义常用错误
+ * 
+ */
+function TypeError(_functionName){
+    return new Error("at: "+_functionName + "\n\t 参数类型不一致!");
+}
+
 var TimeAxis= (function (){
     //alert(__window.document.baseURI);
 
@@ -31,6 +51,67 @@ var TimeAxis= (function (){
         this.y = _y || 0;
     }
 
+    function ZSlider(_x,_y,_w,_h,_r){
+        this.SVGDom = document.createElementNS("http://www.w3.org/2000/svg","polygon");
+        this.baseX = _x || 0;
+        this.baseY = _y || 0;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.width = _w || 0;
+        this.height = _h || 0;
+        this.rat = _r || 0.0;
+        this.isClick = false;
+        
+        __SVGDomChilds.push(this);
+
+        this.SVGDom.setAttribute("points",
+            this.baseX.toString()+","+this.baseY.toString()+" "+
+            (this.baseX+this.width/2).toString()+","+(this.baseY+this.height*this.rat).toString()+" "+
+            (this.baseX+this.width/2).toString()+","+(this.baseY+this.height).toString()+" "+
+            (this.baseX-this.width/2).toString()+","+(this.baseY+this.height).toString()+" "+
+            (this.baseX-this.width/2).toString()+","+(this.baseY+this.height*this.rat).toString()
+        );
+
+        this.SVGDom.onmousedown = function (e){
+            this.isClick = true;
+            var tmpslider = findSlider(this);
+            tmpslider.offsetX = e.offsetX;// - tmpslider.baseX;
+            tmpslider.offsetY = e.offsetY;// - tmpslider.baseY;
+            
+        }
+        this.SVGDom.onmouseup = function(){
+            this.isClick = false;
+        }
+        this.SVGDom.onmousemove = function (e){
+            if(this.isClick){//debugger;
+                var tmpslider = findSlider(this);
+                console.log("e:"+e.offsetX+","+e.offsetY);
+                console.log("this.last:"+tmpslider.offsetX+","+tmpslider.offsetY);
+                
+                tmpslider.baseX += (e.offsetX - tmpslider.offsetX);
+                tmpslider.baseY += (e.offsetY - tmpslider.offsetY);
+                console.log("this.base:"+tmpslider.baseX+","+tmpslider.baseY);
+
+                tmpslider.offsetX = e.offsetX;// - tmpslider.baseX;
+                tmpslider.offsetY = e.offsetY;// - tmpslider.baseY;
+
+                if( tmpslider!= null )
+                    tmpslider.ReMoveAndReSize();
+                //tmpslider.offsetX = tmpslider.baseX;
+                //tmpslider.offsetY = tmpslider.baseY;
+            }
+        }
+
+    }
+    ZSlider.prototype.ReMoveAndReSize= function (){
+        this.SVGDom.setAttribute("points",
+            this.baseX.toString()+","+this.baseY.toString()+" "+
+            (this.baseX+this.width/2).toString()+","+(this.baseY+this.height*this.rat).toString()+" "+
+            (this.baseX+this.width/2).toString()+","+(this.baseY+this.height).toString()+" "+
+            (this.baseX-this.width/2).toString()+","+(this.baseY+this.height).toString()+" "+
+            (this.baseX-this.width/2).toString()+","+(this.baseY+this.height*this.rat).toString()
+        );
+    }
 /**
  * 私有变量定义
  * 
@@ -42,6 +123,16 @@ var TimeAxis= (function (){
  * 私有函数定义
  * 
  */
+    function findSlider(obj){
+        if(! obj instanceof SVGPolygonElement)
+            throw TypeError("findSlider");
+        for(var i=0;i<__SVGDomChilds.length;i++){
+            if(__SVGDomChilds[i].SVGDom === obj)
+                return __SVGDomChilds[i];
+            else
+                return null;
+        }
+    }
     function setAttr(_svgdom,key,value){
         if(! _svgdom instanceof SVGElement)
             throw Error("设置属性的对象必须是一个SVG元素.");
@@ -49,10 +140,16 @@ var TimeAxis= (function (){
             throw Error("属性必须是字符串.");
 
     }
-    function createSlider(_x,_y){
-        var ix,iy;  //滑块指示点的坐标
-        var iw = 10,ih = 20;  //滑块的宽和高
-        var irat = 0.5;   //滑块上下两部分所占的比例 [0 - 1]
+
+    function _polygonReMoveAndReSize(obj){
+        if(! obj instanceof SVGPolygonElement)
+            throw TypeError("_polygonReMoveAndReSize");
+    }
+
+    function createSlider(_x,_y){debugger;
+        var ix,iy;              //滑块指示点的坐标
+        var iw = 10,ih = 20;    //滑块的宽和高
+        var irat = 0.5;         //滑块上下两部分所占的比例 [0 - 1]
         var islider = document.createElementNS("http://www.w3.org/2000/svg","polygon");
         
         if( _x instanceof Point){
@@ -62,19 +159,34 @@ var TimeAxis= (function (){
             ix = _x || 0;
             iy = _y || 0;
         }
+
+        islider.z_baseX = ix;
+        islider.z_baseY = iy;
         
         islider.setAttribute("points",
-            ix.toString()+","+iy.toString()+" "+
-            (ix+iw/2).toString()+","+(iy+ih*irat).toString()+" "+
-            (ix+iw/2).toString()+","+(iy+ih).toString()+" "+
-            (ix-iw/2).toString()+","+(iy+ih).toString()+" "+
-            (ix-iw/2).toString()+","+(iy+ih*irat).toString()
+            islider.z_baseX.toString()+","+islider.z_baseY.toString()+" "+
+            (islider.z_baseX+iw/2).toString()+","+(islider.z_baseY+ih*irat).toString()+" "+
+            (islider.z_baseX+iw/2).toString()+","+(islider.z_baseY+ih).toString()+" "+
+            (islider.z_baseX-iw/2).toString()+","+(islider.z_baseY+ih).toString()+" "+
+            (islider.z_baseX-iw/2).toString()+","+(islider.z_baseY+ih*irat).toString()
         );
-
+        islider.isClick = false;
         
+        islider.onmousedown = function (){
+            islider.isClick = true;
+        }
+        islider.onmouseup = function(){
+            islider.isClick = false;
+        }
+        islider.onmousemove = function (){
+            if(islider.isClick){
+
+            }
+        }
 
         return islider;
     }
+    
     function areaInit(_id){
         if(typeof _id === 'undefined')
             __SVGDom = document.getElementById("TimeAxis");
@@ -85,7 +197,7 @@ var TimeAxis= (function (){
     return function (id){debugger;
         this.aa = 1;  
         areaInit(id);
-        __SVGDom.appendChild(createSlider(new Point(10,20)));
+        __SVGDom.appendChild(new ZSlider(10,10,20,20,0.5).SVGDom);
     
     }
 
